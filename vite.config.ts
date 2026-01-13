b793424201de26d1466f82af75c5662ed736b2f1
morvicool <morvicool@gmail.com>
2026-01-13 15:44:05 +0300

    adaptid cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
-import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [fresh(), tailwindcss(), cloudflare()],
